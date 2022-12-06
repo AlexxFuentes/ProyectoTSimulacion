3ac248fd444330ec03b67926c8c9fdbb02560800
Alexx Fuentes <65086190+AlexxFuentes@users.noreply.github.com>
2022-12-06 12:26:47 -0600

    cambios en eventos, modificacion de las funciones de numeros aleatorios

--- ProyectoTsimulacion/eventos/eventos.go
+++ ProyectoTsimulacion/eventos/eventos.go
@@ -15,13 +15,12 @@ Funcion que retorna la cantidad de personas que fueron atendidas
 durante un intervalos de tiempo a una frecuencia especifica
 
 parametros: 
-	intervalo de tiempo en min 
-	frecuencia por min
+	intervalo de tiempo en min	: int
+	frecuencia por min			: float64
 */
 func Frecuencia_personas(intervalo_tiempo int, frecuencia_min float64) int {
 	var (
-		cont_personas = 0
-		i             = 0
+		cont_personas, i = 0, 0
 	)
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 	for i < intervalo_tiempo {
@@ -38,15 +37,14 @@ func Frecuencia_personas(intervalo_tiempo int, frecuencia_min float64) int {
 Funcion que retorna el total de personas atendidas en todo un dia
 
 Parametros: 
-	numero de dias
+	numero de dias : int
 */
 func Personas_llegaron(dias int) map[int]int {
 	var (
 		intervalo_tiempo             = []int{180, 180, 90, 90, 240}            //Intervalo de tiempo en minutos
 		frecuencia_min               = []float64{0.31, 0.46, 0.55, 0.23, 0.73} // frecuencia por minuto
 		intervalo_horas              = []string{"04:30AM a 07:30AM", "07:31AM a 10:30AM", "10:31AM a 12:00M", "12:00AM a 01:30PM", "01:31PM a 05:30PM"}
-		frecuencia                   = 0
-		frecuencia_hora              = 0
+		frecuencia, frecuencia_hora  = 0, 0
 		frecuencia_personas_llegaron = make(map[int]int)
 	)
 
@@ -69,7 +67,9 @@ func Personas_llegaron(dias int) map[int]int {
 Funcion que devuelve un numero aleatorio entre [5, 10] minutos
 simulando el tiempo de atencion de una persona.
 
-Parametros: min= 5, max = 11
+Parametros: 
+	min : int
+	max : int
 */
 func Tiempo_atencion(min, max int) int {
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
@@ -80,7 +80,11 @@ func Tiempo_atencion(min, max int) int {
 Fucion que simula la cantidad de personas atendidas segun las que hayan llegado
 en un un dia, de acuerdo a la cantidad de estaciones y numero de recursos asignados
 
-Parametros: estaciones, recursos, dias, personas que llegaron por dia
+Parametros: 
+	estaciones					  : int
+	recursos					  : int
+	dias 						  : int
+	personas que llegaron por dia : map
 */
 func Personas_atendidas(estaciones, recursos, dias int, personas_llegaron_dias map[int]int) {
 	var (
@@ -154,8 +158,7 @@ func Personas_atendidas(estaciones, recursos, dias int, personas_llegaron_dias m
 		fmt.Printf("Estaciones despues de atender a las personas durante el dia %d \n", dia)
 		imprimir(asignar_estacion_personas_dia, "no atendidas")
 		fmt.Printf("Numero de personas que llegaron el dia %d: %d \n", dia, personas_llegaron_dias[dia])
-		fmt.Printf("Numero de personas que no se atendieron el dia %d: %d \n", dia, verificar(asignar_estacion_personas_dia))
-		fmt.Printf("\n\n")
+		fmt.Printf("Numero de personas que no se atendieron el dia %d: %d \n\n\n", dia, verificar(asignar_estacion_personas_dia))
 	}
 }
 
@@ -163,9 +166,11 @@ func Personas_atendidas(estaciones, recursos, dias int, personas_llegaron_dias m
 Funcion que asigna de forma aleatoria las personas que llegaron
 a las estaciones disponibles
 
-Parametros: numero de estaciones, Objetivo: numero de personas que vamos a dividir de forma aleatoria en las estaciones
+Parametros: 
+	numero de estaciones																	: int
+	Objetivo: numero de personas que vamos a dividir de forma aleatoria en las estaciones	: int
 */
-func Asignar_personas_estaciones(num_estaciones int, objetivo int) map[int]int {
+func Asignar_personas_estaciones(num_estaciones , objetivo int) map[int]int {
 	var (
 		est            = make(map[int]int)
 		sum_est        int
@@ -195,7 +200,7 @@ Funcion que imprime de forma ordenada como
 fueron asignada las personas en las estaciones
 
 Parametros: 
-	m: map donde se guarda el numero de estacion y el numero de personas por estacion
+	m: map donde se guarda el numero de estacion y el numero de personas por estacion 
 	s: cadena de texto con algun mensaje para personalizar la salida
 */
 func imprimir(m map[int]int, s string) {
